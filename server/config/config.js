36eb4db0ce8f8970bcfc10c499c90317d163a862
unknown <fabiantunez90@gmail.com>
2022-06-20 18:22:42 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 }else{
-    urlDB = 'mongodb+srv://Fabiantunez:Faby147787@cluster0.hdeutof.mongodb.net/?retryWrites=true&w=majority'
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
